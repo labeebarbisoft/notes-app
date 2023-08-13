e4fdcd0a740159bb4df179eea8c4a87c7b74d910
labeebarbisoft <muhammad.labeeb@arbisoft.com>
2023-08-13 11:43:05 +0500

    implemented database for user login and signup

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, render_template, redirect, url_for, flash
 from website.forms import LoginForm, RegistrationForm
+from .models import User
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint("auth", __name__)
 
@@ -8,21 +11,31 @@ auth = Blueprint("auth", __name__)
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        user = form.username.data
-        return redirect(url_for("views.home", user=user))
-    return render_template("login.html", form=form)
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or password", category="error")
+            return redirect(url_for("auth.login"))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for("views.home", user=current_user))
+    return render_template("login.html", form=form, user=current_user)
 
 
 @auth.route("/logout", methods=["GET", "POST"])
+@login_required
 def logout():
-    return "logout"
+    logout_user()
+    return redirect(url_for("auth.login", user=current_user))
 
 
 @auth.route("/sign-up", methods=["GET", "POST"])
 def signup():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = form.username.data
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
         flash("Account created.", category="success")
-        return redirect(url_for("auth.login"))
-    return render_template("sign_up.html", form=form)
+        login_user(user, remember=False)
+        return redirect(url_for("views.home", user=current_user))
+    return render_template("sign_up.html", form=form, user=current_user)
--- website/forms.py
+++ website/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from .models import User
 
 
 class LoginForm(FlaskForm):
@@ -41,12 +42,12 @@ class RegistrationForm(FlaskForm):
     )
     submit = SubmitField("Register")
 
-    # def validate_username(self, username):
-    #     user = User.query.filter_by(username=username.data).first()
-    #     if user is not None:
-    #         raise ValidationError("Please use a different username.")
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError("Please use a different username.")
 
-    # def validate_email(self, email):
-    #     user = User.query.filter_by(email=email.data).first()
-    #     if user is not None:
-    #         raise ValidationError("Please use a different email address.")
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError("Please use a different email address.")
